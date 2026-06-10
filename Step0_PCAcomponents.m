662fa3b87a0b7fb5a1cdbc5089406389c05b7c96
Nazarii Salish <nazarii.salish@gmail.com>
2026-06-10 10:43:47 +0200

    Analysis of DynScores
    Replace Step 0 with additonal analysis for the score dynamics

--- Step0_PCAcomponents.m
+++ Step0_PCAcomponents.m
@@ -1,19 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Forecasts Comparison
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all;
 clc;
 
-%% Short Info:
-% Analyzez strucutre of the PCA
-
-%% Add Libraries and Data
+%% Add Libraries
 addpath AddFunc
 addpath Data
 
-%% Step 1: Read Data and Project
-load('Data\SeasonAdjData');
-ConstrReg = csvread('Data/GeoConstraints/DE_Constraints.csv');
-mkdir('Outputs');  % creates a folder to save all of the outputs 
-Threshold = 0.75;
+%% Step 1: Read Data
+% This step loads seasonally adjusted ozone concentration data observed
+% at different stations, the surface data (created in Step 1), and the
+% geographic borders of Germany
 
+load('Data\SeasonAdjData'); % Data on the grid seasonaly addjusted
+load('Data\FTSs');          % Data given in the surface form (Step 1 has to be executed for that)
+ConstrReg = csvread('Data/GeoConstraints/DE_Constraints.csv');
+
+% Projected Coordinates
+Threshold = 0.75;
 % project data on the plain (Gauss-Krüger Zone 3)
 wgs84           = geocrs(4326);
 proj            = projcrs(31467);
@@ -21,44 +26,55 @@ proj            = projcrs(31467);
 [x, y]          = projfwd(proj, ConstrReg(:,1), ConstrReg(:,2));
 ConstrReg       = [y,x];
 
-%% Step 2: Triangulation of the selected geographic area
-% This step defines the geographical area for our analysis. In particular, 
-% it creates a triangulation of the area for each variable and generates Figure 7 in the paper's appendix.
-
-DTOzone     = delaunayTriangulation([LonOz,LatOz]);
-CleanDTOz   = CleanTriangulation(DTOzone,[ConstrReg(:,2),ConstrReg(:,1)],Threshold);
-% [RemP, regP] = TriangStats(DTOzone, [ConstrReg(:,2),ConstrReg(:,1)], Threshold);
-[OzoneFTS,OzoneCoef,OzoneBasis]    = Grid2Func(Ozone,DTOzone,CleanDTOz);
-%% Plots 
-% This block obtains additional information about the characteristics of our surface data.
-
-K_max       = 15;    
-pcastrOzone = pca3D(OzoneFTS(1:200), K_max, 1);   
-% plot a scree plot of explaind variance for an intuition
-h = findobj('type','figure');
-n = length(h);
+[~,T]           = size(OzoneS);
+K_max           = 15;
+T_tr            = 200;
+OzoneFTS        = fd(OzoneCoef(:,1:T_tr),OzoneBasis);
+    
+%% Static and dynamic scores
+q_dyn           = 2; % cumulative autocovariance    
+dyn_scs         = DynamScoresSurf({OzoneCoef(:,1:T_tr),OzoneBasis}, K_max, 1, q_dyn); 
+stat_scs        = pca3D({OzoneCoef(:,1:T_tr),OzoneBasis}, K_max, 1); 
 
 % scree plots
+fg1 = figure(1);
+    plot(dyn_scs.values(1:K_max));
+    title('Scree plot');
 
+% comapre with static scores
 figure(2);
-subplot(1,2,1)
-    plot(pcastrOzone.varprop(1:K_max));
-    title('Ozone');
-subplot(1,2,2)    
-    plot(pcastrOzone.values(1:K_max));
-
-fh=figure(3);
 subplot(2,2,1)
-    autocorr(pcastrOzone.pcascr(:,1));
+    plot(stat_scs.varprop(1:K_max));
+    title('Static Scores: Prop');
+subplot(2,2,2)    
+    plot(stat_scs.values(1:K_max));
+    title('Static Scores: Vals');
+subplot(2,2,3)
+    plot(dyn_scs.varprop(1:K_max));
+    title('Dynamic Scores: Prop');
+subplot(2,2,4)    
+    plot(dyn_scs.values(1:K_max));
+    title('Dynamic Scores: Vals');
+
+
+% Autocovariances     
+fg2=figure(3);
+subplot(2,2,1)
+    autocorr(dyn_scs.scr(:,1));
     title('1st Score Series');
 subplot(2,2,2)
-    autocorr(pcastrOzone.pcascr(:,2));
+    autocorr(dyn_scs.scr(:,2));
     title('2nd Score Series');
 subplot(2,2,3)
-    autocorr(pcastrOzone.pcascr(:,3));
+    autocorr(dyn_scs.scr(:,3));
     title('3rd Score Series');
 subplot(2,2,4)
-    autocorr(pcastrOzone.pcascr(:,4));
+    autocorr(dyn_scs.scr(:,4));
     title('4th Score Series');
 
-exportgraphics(fh, ['Outputs/AutocorrScores.pdf'], 'BackgroundColor', 'none', 'Resolution', 300);
+
+outFolder = fullfile(pwd, 'Outputs');
+if ~exist(outFolder, 'dir')
+    mkdir(outFolder);
+end
+exportgraphics(fg1, ['Outputs/ScreePlot.pdf'], 'BackgroundColor', 'none', 'Resolution', 300);
