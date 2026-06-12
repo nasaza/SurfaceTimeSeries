3b57e903d11d4265fa74ecbbfd01b40cf73a0bd4
Nazarii Salish <nazarii.salish@gmail.com>
2026-06-12 17:26:27 +0200

    Last update before converting to new version
    Fixing some last stuff before converting

--- Step1_5AdditionalScoreAnn.m
+++ Step1_5AdditionalScoreAnn.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Forecasts Comparison
+%% Aditional Analysis
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all;
 clc;
@@ -32,7 +32,7 @@ T_tr            = 200;
 OzoneFTS        = fd(OzoneCoef(:,1:T_tr),OzoneBasis);
     
 %% Static and dynamic scores
-q_dyn           = 2; % cumulative autocovariance    
+q_dyn           = 3; % cumulative autocovariance    
 dyn_scs         = DynamScoresSurf({OzoneCoef(:,1:T_tr),OzoneBasis}, K_max, 1, q_dyn); 
 stat_scs        = pca3D({OzoneCoef(:,1:T_tr),OzoneBasis}, K_max, 1); 
 
@@ -59,18 +59,46 @@ subplot(2,2,4)
 
 % Autocovariances     
 fg2=figure(3);
-subplot(2,2,1)
+subplot(2,3,1)
     autocorr(dyn_scs.pcascr(:,1));
     title('1st Score Series');
-subplot(2,2,2)
+subplot(2,3,2)
     autocorr(dyn_scs.pcascr(:,2));
     title('2nd Score Series');
-subplot(2,2,3)
+subplot(2,3,3)
     autocorr(dyn_scs.pcascr(:,3));
     title('3rd Score Series');
-subplot(2,2,4)
+subplot(2,3,4)
     autocorr(dyn_scs.pcascr(:,4));
     title('4th Score Series');
+subplot(2,3,5)
+    autocorr(dyn_scs.pcascr(:,5));
+    title('5th Score Series');
+subplot(2,3,6)
+    autocorr(dyn_scs.pcascr(:,6));
+    title('6th Score Series');    
+
+% Partial Autocovariances     
+figure(4);
+subplot(2,3,1)
+    parcorr(dyn_scs.pcascr(:,1));
+    title('1st Score Series');
+subplot(2,3,2)
+    parcorr(dyn_scs.pcascr(:,2));
+    title('2nd Score Series');
+subplot(2,3,3)
+    parcorr(dyn_scs.pcascr(:,3));
+    title('3rd Score Series');
+subplot(2,3,4)
+    parcorr(dyn_scs.pcascr(:,4));
+    title('4th Score Series');
+subplot(2,3,5)
+    parcorr(dyn_scs.pcascr(:,5));
+    title('5th Score Series');
+subplot(2,3,6)
+    parcorr(dyn_scs.pcascr(:,6));
+    title('6th Score Series');    
+
 
 
 outFolder = fullfile(pwd, 'Outputs');
@@ -78,3 +106,89 @@ if ~exist(outFolder, 'dir')
     mkdir(outFolder);
 end
 exportgraphics(fg1, ['Outputs/ScreePlot.pdf'], 'BackgroundColor', 'none', 'Resolution', 300);
+
+
+%% Loading surfaces comparison: PCA vs Dynamic Scores
+
+% Prepare Germany border
+Region     = [ConstrReg(:,2),ConstrReg(:,1)];
+RegionBord = polyshape(Region);
+
+% Extract loading coefficients
+stat_load_coef = getcoef(stat_scs.pcafd);
+dyn_load_coef  = getcoef(dyn_scs.pcafd);
+
+% First two PCA loading surfaces
+stat_load_1 = fd(stat_load_coef(:,1), OzoneBasis);
+stat_load_2 = fd(stat_load_coef(:,2), OzoneBasis);
+
+% First two dynamic loading surfaces
+dyn_load_1  = fd(dyn_load_coef(:,1), OzoneBasis);
+dyn_load_2  = fd(dyn_load_coef(:,2), OzoneBasis);
+
+% Common color scale based on values at ozone stations
+vals_load = [ ...
+    eval_FEM_fd(LonOz,LatOz,stat_load_1); ...
+    eval_FEM_fd(LonOz,LatOz,stat_load_2); ...
+    eval_FEM_fd(LonOz,LatOz,dyn_load_1); ...
+    eval_FEM_fd(LonOz,LatOz,dyn_load_2)];
+
+cmax_load = max(abs(vals_load),[],'all');
+
+% Plot
+fg5 = figure(5);
+fg5.Position = [100, 100, 1100, 700];
+
+subplot(2,2,1)
+    hold on
+    plot(RegionBord, 'FaceColor', 'none');
+    plot(stat_load_1, [], [], [], 100);
+    hold off
+    axis equal tight
+    view(2)
+    colormap(jet)
+    clim([-cmax_load cmax_load])
+    colorbar
+    title('PCA: 1st loading')
+
+subplot(2,2,2)
+    hold on
+    plot(RegionBord, 'FaceColor', 'none');
+    plot(stat_load_2, [], [], [], 100);
+    hold off
+    axis equal tight
+    view(2)
+    colormap(jet)
+    clim([-cmax_load cmax_load])
+    colorbar
+    title('PCA: 2nd loading')
+
+subplot(2,2,3)
+    hold on
+    plot(RegionBord, 'FaceColor', 'none');
+    plot(dyn_load_1, [], [], [], 100);
+    hold off
+    axis equal tight
+    view(2)
+    colormap(jet)
+    clim([-cmax_load cmax_load])
+    colorbar
+    title('DS: 1st loading')
+
+subplot(2,2,4)
+    hold on
+    plot(RegionBord, 'FaceColor', 'none');
+    plot(dyn_load_2, [], [], [], 100);
+    hold off
+    axis equal tight
+    view(2)
+    colormap(jet)
+    clim([-cmax_load cmax_load])
+    colorbar
+    title('DS: 2nd loading')
+
+exportgraphics(fg5, ...
+    fullfile(outFolder,'LoadingSurfaces_PCA_DS.pdf'), ...
+    'BackgroundColor', 'none', ...
+    'Resolution', 300, ...
+    'ContentType', 'vector');
--- Step3_Forecasting_DynamicScores.m
+++ Step3_Forecasting_DynamicScores.m
@@ -46,22 +46,11 @@ K_max       = 10;
 p_max       = 5;   % Maximum nuber of lag considered in the analysis
 KNN_max     = 50;  % Maximum number of KNN in the calibraiton
 KNNs        = zeros(H,2);
+q_dyn       = 3;
 
 FEMBasOz    = OzoneBasis;
 
-MSE_MP      = zeros(N_Fors,1); 
-MSE_NP      = zeros(N_Fors,1); 
-MSE_RF      = zeros(N_Fors,1); 
-MSE_FAR1    = zeros(N_Fors,1);
-MSE_PCA_VAR = zeros(N_Fors,1);
-MSE_PCA_KNN = zeros(N_Fors,1);
-MSE_DS_VAR  = zeros(N_Fors,1);
-MSE_DS_KNN  = zeros(N_Fors,1);
-MSE_MP_VAR  = zeros(N_Fors,1);
-MSE_MP_KNN  = zeros(N_Fors,1);
-
 % Variables to store MSE surfaces from different predictors
-
 MSurf_MP      = zeros(size(OzoneS,1),(H+1-h)); 
 MSurf_NP      = zeros(size(OzoneS,1),(H+1-h)); 
 MSurf_RF      = zeros(size(OzoneS,1),(H+1-h)); 
@@ -74,8 +63,10 @@ MSurf_MP_VAR  = zeros(size(OzoneS,1),(H+1-h));
 MSurf_MP_KNN  = zeros(size(OzoneS,1),(H+1-h)); 
 
 % Number of components and lags considered 
-L_set = [3 1 1 1 1]; % Ozone, Sun, Precip, Temp, Wind
-m_set = [2 1 1 1 1];
+Static_L_set = [3 1 1 1 1]; % Ozone, Sun, Precip, Temp, Wind
+Static_m_set = [2 1 1 1 1];
+Dyn_L_set    = [4      1 1 1 1]; 
+Dyn_m_set    = [3 1 1 1 1];
 
 
 %% Step 3: Forecasting with Different Methods
@@ -90,9 +81,9 @@ for i=1:(H+1-h)
     TrueVal     = OzoneS(:,nobs_i+h);    
     OzoneFTSi   = fd(OzoneCoef(:,1:nobs_i),OzoneBasis);
     
-%% Functional Persepctive (FP): fPCA for the available observations T-(H-1)+i
+%% Static and dynamic scores
     
-    DynamScores = DynamScoresSurf({OzoneCoef(:,1:nobs_i),OzoneBasis}, K_max, 1, 2); 
+    dynstrOz_i  = DynamScoresSurf({OzoneCoef(:,1:nobs_i),OzoneBasis}, K_max, 1, q_dyn); 
     pcastrOz_i  = pca3D({OzoneCoef(:,1:nobs_i),OzoneBasis}, K_max, 1); 
     pcastrS_i   = pca3D({SunCoef(:,1:nobs_i),SunBasis}, K_max, 1); 
     pcastrP_i   = pca3D({PrecipCoef(:,1:nobs_i),PrecipBasis}, K_max, 1); 
@@ -100,184 +91,204 @@ for i=1:(H+1-h)
     pcastrW_i   = pca3D({WindCoef(:,1:nobs_i),WindBasis}, K_max, 1); 
     PCA_addreg  = {pcastrS_i,pcastrP_i,pcastrT_i,pcastrW_i};
     
-%% Method 1: (FP) Mean Predictor
+% %% Method 1: Mean Predictor
+% 
+%     MSurf_MP(:,i) = TrueVal-mean(OzoneS(:,1:nobs_i),2);
+% 
+% %% Method 2: Naive Predictor
+% 
+%     MSurf_NP(:,i) = TrueVal-OzoneS(:,nobs_i);    
+% 
+% %% Method 3: FAR(1)
+% 
+%     FuncPredCoeff   = SFAR1(pcastrOz_i,Static_L(1),h);
+%     FuncPred        = fd(FuncPredCoeff,FEMBasOz);
+%     FPointEval      = eval_FEM_fd(LonOz,LatOz,FuncPred);
+%     MSurf_FAR1(:,i)     = TrueVal-FPointEval;
+% 
+%% Method 4: PCA+VAR
 
-    MSE_MP(i)   = mean((TrueVal-mean(OzoneS(:,1:nobs_i),2)).^2);
-    MSurf1(:,i) = TrueVal-mean(OzoneS(:,1:nobs_i),2);
-
-%% Method 2: (FP) Naive Predictor
-
-    MSE_NP(i)   = mean((TrueVal-OzoneS(:,nobs_i)).^2);
-    MSurf2(:,i) = TrueVal-OzoneS(:,nobs_i);
-    
-    
-%% Method 3: (FP) FAR(1)
-
-    FuncPredCoeff   = SFAR1(pcastrOz_i,5,h);
+    FuncPredCoeff   = FFM_VARX(pcastrOz_i,PCA_addreg,Static_L_set,Static_m_set,h);
     FuncPred        = fd(FuncPredCoeff,FEMBasOz);
     FPointEval      = eval_FEM_fd(LonOz,LatOz,FuncPred);
-    MSE_FAR1(i)     = mean((TrueVal-FPointEval).^2); 
-    MSurf3(:,i)     = TrueVal-FPointEval;
+    MSurf_PCA_VAR(:,i)     = TrueVal-FPointEval;
+
+% %% Method 5: PCA+KNN
+% %-> Part 1: Estimating number of neigbours
+% 
+%     K_NNv        = zeros(KNN_max,1) ;
+%     pcastrKNN_i  = pca3D({OzoneCoef(:,1:nobs_i-h),OzoneBasis}, K_max, 1); 
+%     for knn = 1:KNN_max
+%         FuncPredCoeff  = FFM_KNN(pcastrKNN_i,Static_L_set(1),Static_m_set(1),knn,h);
+%         FuncPred       = fd(FuncPredCoeff,FEMBasOz);
+%         FPointEval     = eval_FEM_fd(LonOz,LatOz,FuncPred);
+%         K_NNv(knn,1)   = mean((OzoneS(:,nobs_i)-FPointEval).^2);
+%     end
+%     [~,K_min]     = min(K_NNv);
+%     KNNs(i,:)     = K_min;
+% %-> Part 2    
+%     FuncPredCoeff  = FFM_KNN(pcastrOz_i,Static_L_set(1),Static_m_set(1),K_min,h);
+%     FuncPred       = fd(FuncPredCoeff,FEMBasOz);
+%     FPointEval     = eval_FEM_fd(LonOz,LatOz,FuncPred);
+%     MSurf_PCA_KNN(:,i)    = TrueVal-FPointEval;
+
+%% Method 6: DS+VAR
+
+    FuncPredCoeff = FFM_VARX(dynstrOz_i,PCA_addreg,Dyn_L_set,Dyn_m_set,h);
+    FuncPred      = fd(FuncPredCoeff,FEMBasOz);
+    FPointEval    = eval_FEM_fd(LonOz,LatOz,FuncPred);
+    MSurf_DS_VAR(:,i) = TrueVal - FPointEval;
+
+% %% Method 7: DS+KNN
+%     %-> Part 1: Estimating number of neighbours
+% 
+%     K_NNv       = zeros(KNN_max,1);
+%     dynstrKNN_i = DynamScoresSurf({OzoneCoef(:,1:nobs_i-h),OzoneBasis}, ...
+%                                    K_max, 1, q_dyn);
+%     for knn = 1:KNN_max
+%         FuncPredCoeff = FFM_KNN(dynstrKNN_i,Dyn_L_set(1),Dyn_m_set(1),knn,h);
+%         FuncPred      = fd(FuncPredCoeff,FEMBasOz);
+%         FPointEval    = eval_FEM_fd(LonOz,LatOz,FuncPred);
+%         K_NNv(knn,1)  = mean((OzoneS(:,nobs_i)-FPointEval).^2);
+% 
+%     end
+%     [~,K_min]   = min(K_NNv);
+%     KNNDS(i,1)  = K_min;
+% 
+%     %-> Part 2: Final DS KNN forecast
+%     FuncPredCoeff = FFM_KNN(dynstrOz_i,Dyn_L_set(1),Dyn_m_set(1),K_min,h);
+%     FuncPred      = fd(FuncPredCoeff,FEMBasOz);
+%     FPointEval    = eval_FEM_fd(LonOz,LatOz,FuncPred);
+%     MSurf_DS_KNN(:,i) = TrueVal - FPointEval;
+
     
-%% Method 4: (FP) Linear Forecast with scree plot estimates
+ % %% Method 8: MP+VAR
+ %    FPredGrid = DFFM_VARX_GRID(OzoneS(:,1:nobs_i), WindS(:,1:nobs_i), ...
+ %                               SunS(:,1:nobs_i), PrecipS(:,1:nobs_i), ...
+ %                               TempS(:,1:nobs_i), Static_L_set,Static_m_set,h); 
+ %    MSurf_MP_VAR(:,i) = TrueVal - FPredGrid;
+ % 
+ %    %% Method 9: MP+KNN
+ %    %-> Part 1: Estimating number of neighbours
+ %    K_NNv       = zeros(KNN_max,1);
+ %    pcastr_i    = FactorDecompFTSGrid(OzoneS(:,1:nobs_i-h), K_max, 1);
+ % 
+ %    for knn = 1:KNN_max
+ %        FPredGrid    = DFFM_KNN_GRID(pcastr_i,Static_L_set(1),Static_m_set(1),knn);
+ %        K_NNv(knn,1) = mean((OzoneS(:,nobs_i)-FPredGrid).^2);
+ %    end
+ % 
+ %    [~,K_min]  = min(K_NNv);
+ %    KNNGs(i,1) = K_min;
+ % 
+ %    %-> Part 2: Final MP KNN forecast
+ % 
+ %    pcastr_i  = FactorDecompFTSGrid(OzoneS(:,1:nobs_i), K_max, 1);
+ %    FPredGrid = DFFM_KNN_GRID(pcastr_i,Static_L_set(1),Static_m_set(1),K_min);
+ %    MSurf_MP_KNN(:,i) = TrueVal - FPredGrid;
+ % 
+ % 
 
-    FuncPredCoeff   = FFM_VARX(pcastrOz_i,PCA_addreg,L_set,m_set,h);
-    FuncPred        = fd(FuncPredCoeff,FEMBasOz);
-    FPointEval      = eval_FEM_fd(LonOz,LatOz,FuncPred);
-    MSE_LF_FP(i)    = mean((TrueVal-FPointEval).^2);
-    MSurf4(:,i)     = TrueVal-FPointEval;
+%% Method 10: DS+Random Forest
 
-%% Method 5: (Multivariate) Linear Forecast with scree plot estimates
+    % FuncPredCoeff   = FRandomForest(dynstrOz_i,Dyn_L_set(1),h);
+    % FuncPred        = fd(FuncPredCoeff,FEMBasOz);
+    % FPointEval      = eval_FEM_fd(LonOz,LatOz,FuncPred);
+    % MSurf_RF(:,i)   = TrueVal - FPointEval;
 
-    FPredGrid     = DFFM_VARX_GRID(OzoneS(:,1:T-H+i-1),WindS(:,1:T-H+i-1),...
-                        SunS(:,1:T-H+i-1),PrecipS(:,1:T-H+i-1),...
-                        TempS(:,1:T-H+i-1),L_set,m_set,h);
-    MSE_VARX_g(i) = mean((TrueVal-FPredGrid).^2);
-    MSurf5(:,i)   = TrueVal-FPredGrid;
 
-     
-%% Method 6: (FP) Nonlinear Forecast with KNN
-%-> Part 1: Estimating number of neigbours
-
-    K_NNv        = zeros(KNN_max,1) ;
-    pcastrKNN_i  = pca3D({OzoneCoef(:,1:nobs_i-h),OzoneBasis}, K_max, 1); 
-    for knn = 1:KNN_max
-        FuncPredCoeff  = FFM_KNN(pcastrKNN_i,L_set(1),m_set(1),knn,h);
-        FuncPred       = fd(FuncPredCoeff,FEMBasOz);
-        FPointEval     = eval_FEM_fd(LonOz,LatOz,FuncPred);
-        K_NNv(knn,1)   = mean((OzoneS(:,nobs_i)-FPointEval).^2);
-    end
-    [~,K_min]     = min(K_NNv);
-    KNNs(i,:)     = K_min;
-%-> Part 2    
-    FuncPredCoeff  = FFM_KNN(pcastrOz_i,L_set(1),m_set(1),K_min,h);
-    FuncPred       = fd(FuncPredCoeff,FEMBasOz);
-    FPointEval     = eval_FEM_fd(LonOz,LatOz,FuncPred);
-    MSE_KNN(i)     = mean((TrueVal-FPointEval).^2);
-    MSurf6(:,i)    = TrueVal-FPointEval;
-     
-    
-%% Method 7: (MP) Nonlinear Forecast with KNN 
-% to be reconstructed
-%-> Part 1
-    K_NNv       = zeros(KNN_max,1);
-    pcastr_i    = FactorDecompFTSGrid(OzoneS(:,1:T-H+i-2), K_max, 1);
-    for knn = 1:KNN_max
-        FPredGrid      = DFFM_KNN_GRID(pcastr_i,L_set(1),m_set(1),knn);
-        K_NNv(knn,1)   = mean((OzoneS(:,T-H+i-1)-FPredGrid).^2);
-    end
-    [~,K_min]      = min(K_NNv);
-    KNNGs(i,:)     = K_min;
-% %-> Part 2
-    pcastr_i       = FactorDecompFTSGrid(OzoneS(:,1:T-H+i-1), K_max, 1);
-    FPredGrid      = DFFM_KNN_GRID(pcastr_i,L_set(1),m_set(1),K_min);
-    MSE_KNNG(i)    = mean((TrueVal-FPredGrid).^2);
-    MSurf7(:,i)    = TrueVal-FPredGrid;      
-
-%% Method 8: (Multivariate) Linear Forecast with Random Forest
-
-    FuncPredCoeff   = FRandomForest(pcastrOz_i,L_set(1),h);
-    FuncPred        = fd(FuncPredCoeff,FEMBasOz);
-    FPointEval      = eval_FEM_fd(LonOz,LatOz,FuncPred);
-    MSE_RF(i)       = mean((TrueVal-FPointEval).^2);
-    MSurf8(:,i)     = TrueVal-FPointEval;
     i
 end
 toc
 
 %% Analyzing the forecasting performance
-% This step compares the performance of different methods based on their MSE 
-% and generates Figures 4 and 5 in the main text of the paper
+% Save the output of this step
 
-% Get the current date and time as a string
 dateStr = datestr(now, 'yyyymmdd_HHMMSS');
 save(['Outputs\Step2_Forcasts',num2str(h),'_',dateStr,'.mat'])
- 
-mean([MSE_MP,MSE_NP,MSE_FAR1,MSE_LF_FP,MSE_VARX_g,MSE_KNN,MSE_KNNG,MSE_RF])
+
+%% Calculate MSE from stored forecast error surfaces
+
+MSurf = {MSurf_MP, MSurf_NP, MSurf_FAR1, MSurf_PCA_VAR, MSurf_PCA_KNN, ...
+         MSurf_DS_VAR, MSurf_DS_KNN, MSurf_MP_VAR, MSurf_MP_KNN, MSurf_RF};
+
+MSE_MP      = mean(MSurf_MP.^2,1)';
+MSE_NP      = mean(MSurf_NP.^2,1)';
+MSE_FAR1    = mean(MSurf_FAR1.^2,1)';
+MSE_PCA_VAR = mean(MSurf_PCA_VAR.^2,1)';
+MSE_PCA_KNN = mean(MSurf_PCA_KNN.^2,1)';
+MSE_DS_VAR  = mean(MSurf_DS_VAR.^2,1)';
+MSE_DS_KNN  = mean(MSurf_DS_KNN.^2,1)';
+MSE_MP_VAR  = mean(MSurf_MP_VAR.^2,1)';
+MSE_MP_KNN  = mean(MSurf_MP_KNN.^2,1)';
+MSE_RF      = mean(MSurf_RF.^2,1)';
+
+MSE_All = [MSE_MP, MSE_NP, MSE_FAR1, MSE_PCA_VAR, MSE_PCA_KNN, MSE_DS_VAR, ...
+           MSE_DS_KNN, MSE_MP_VAR, MSE_MP_KNN, MSE_RF];
+
+Models = {'MF','NF','FAR', 'PCA VAR','PCA KNN', 'DS VAR','DS KNN', ...
+          'MP VAR','MP KNN', 'DS RF'};
+
+mean(MSE_All)
 
 %% Create the figure with the Box plot
+% Plot figure in the paper: box plots of forecasts
 
-% Plor Figure 3 in the paper (box plots of forecasts)
-fh = figure(1);
-FS = 14;
-canvas = [100, 100, 850, 350]; % Adjusted canvas size
-set(fh, 'Position', canvas, 'PaperPositionMode', 'auto');
-% Plot the boxplot
-bh = boxplot([MSE_MP, MSE_NP, MSE_FAR1, MSE_VARX_g, MSE_LF_FP, MSE_KNNG, MSE_KNN,MSE_RF], ...
-            'Labels', {'MP', 'NP', 'FAR', 'LGF', 'LSF', 'NGF', 'NSF','RF'}, 'whisker', 2);
-% Set y-axis limits and font size for labels
-ylim([0 250]);
+fig1    = figure(1);
+FS      = 14;
+canvas  = [100, 100, 1000, 350];
+set(fig1, 'Position', canvas, 'PaperPositionMode', 'auto');
+bh      = boxplot(MSE_All, 'Labels', Models, 'whisker', 2);
 set(bh(7,:), 'Visible', 'off');
 ylabel('MSE', 'FontSize', FS);
+xtickangle(30)
+%Set y-axis limits and font size for labels
+ylim([0 250]);
 
-exportgraphics(fh, ['Outputs/BoxplotsHorizon',num2str(h),'.pdf'], 'BackgroundColor', 'none', 'Resolution', 300);
+exportgraphics(fig1, ['Outputs/BoxplotsHorizon',num2str(h),'.pdf'], ...
+              'BackgroundColor', 'none', 'Resolution', 300);
 
-%% CI surfaces
+%% MSE surfaces plots
+
 DTOzone     = delaunayTriangulation([LonOz,LatOz]);
 CleanDTOz   = CleanTriangulation(DTOzone,[ConstrReg(:,2),ConstrReg(:,1)],Threshold);
 Region      = [ConstrReg(:,2),ConstrReg(:,1)];
 RegionBord  = polyshape(Region);
-
-%--- Input ---
-MSurf = {MSurf1, MSurf2, MSurf3, MSurf4, MSurf5, MSurf6, MSurf7, MSurf8};
-Models = {'MP','NP','FAR','LGF','LSF','NGF','NSF','RF'};
-
-%--- Compute 2.5% quantiles for each model ---
-Q_2_5  = cellfun(@(M) quantile(M, 0.025, 2), MSurf, 'UniformOutput', false);
-Q_97_5 = cellfun(@(M) quantile(M, 0.975, 2), MSurf, 'UniformOutput', false);
-
+MSESurf     = cellfun(@(M) mean(M.^2,2), MSurf, 'UniformOutput', false);
 
 %--- Compute color axis limits across all models ---
-all_lower = cell2mat(Q_2_5');
-all_upper = cell2mat(Q_97_5');
-bottomL   = min(all_lower, [], 'all');
-topL      = max(all_lower, [], 'all');
-bottomU   = min(all_upper, [], 'all');
-topU      = max(all_upper, [], 'all');
+all_mse     = cell2mat(MSESurf');
+bottomMSE   = min(all_mse, [], 'all');
+topMSE      = max(all_mse, [], 'all');
 
-%--- Plot 2.5% and 97.5 Confidence Surfaces ---
-figLow = figure(2);
-figUp  = figure(3);
-for m = 1:8
-    figure(figLow);
-        subplot(2, 4, m)
-        Season = Grid2Func(Q_2_5{m}, DTOzone, CleanDTOz);  % Surface from vector    
-        hold on
+%--- Plot MSE surfaces ---
+
+figMSE2 = figure(2);
+
+for m = 1:length(Models)
+
+    subplot(2,5,m)
+    Season = Grid2Func(MSESurf{m}, DTOzone, CleanDTOz);
+    hold on
         plot(RegionBord, 'FaceColor', 'none');
         plot(Season, [], [], [], 100);
-        hold off
-        axis equal tight;
-        colormap(flipud(jet));
-        % colormap(flipud(redbluecmap));
-        clim([bottomL topL]);
-        colorbar;
-        view(2);
-        title(Models{m});
-        xlabel('Easting');
-        ylabel('Northing');
-     figure(figUp);
-        subplot(2, 4, m)
-        Season = Grid2Func(Q_97_5{m}, DTOzone, CleanDTOz);  % Surface from vector    
-        hold on
-        plot(RegionBord, 'FaceColor', 'none');
-        plot(Season, [], [], [], 100);
-        hold off
-        axis equal tight;
-        colormap((jet));
-        % colormap(redbluecmap);
-        clim([bottomU topU]);
-        colorbar;
-        view(2);
-        title(Models{m});
-        xlabel('Easting');
-        ylabel('Northing');
+    hold off
+    axis equal tight;
+    colormap(jet);
+    clim([bottomMSE topMSE]);
+    colorbar;
+    view(2);
+    title(Models{m});
+    xlabel('Easting');
+    ylabel('Northing');
 
 end
 
-%--- Resize and export figure ---
-figLow.Position = [100, 100, 1200, 600];  
-figUp.Position = [100, 100, 1200, 600]; 
-exportgraphics(figLow, ['Outputs/CI_2p5_Surfaces_Horizon', num2str(h), '.pdf'], ...
-    'BackgroundColor', 'none', 'Resolution', 300, 'ContentType', 'vector');
-exportgraphics(figUp, ['Outputs/CI_97p5_Surfaces_Horizon', num2str(h), '.pdf'], ...
-    'BackgroundColor', 'none', 'Resolution', 300, 'ContentType', 'vector');
+% 
+% %--- Resize and export figure ---
+% figLow.Position = [100, 100, 1200, 600];  
+% figUp.Position = [100, 100, 1200, 600]; 
+% exportgraphics(figLow, ['Outputs/CI_2p5_Surfaces_Horizon', num2str(h), '.pdf'], ...
+%     'BackgroundColor', 'none', 'Resolution', 300, 'ContentType', 'vector');
+% exportgraphics(figUp, ['Outputs/CI_97p5_Surfaces_Horizon', num2str(h), '.pdf'], ...
+%     'BackgroundColor', 'none', 'Resolution', 300, 'ContentType', 'vector');
